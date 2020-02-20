8a0b9f36ea93dc15fc892719b4f035fad09b4356
zhangzulin1234 <1049081426@qq.com>
2020-02-20 22:29:39 +0800

    4-5项目目录结构

---
+++ src/pages/order.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <root-view></root-view>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"nav-header"
+    }
+
+</script>
